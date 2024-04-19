fab2cfee03b68283612297ea09edab35991d3a48
Alexandre Rostovtsev <arostovtsev@google.com>
2024-04-19 15:08:22 -0400

    Remove legacy golden files and their usage

--- update-stardoc-tests.sh
+++ update-stardoc-tests.sh
@@ -46,11 +46,10 @@ fi
 # golden test pattern
 EXCLUDED_TESTS="namespace_test_with_allowlist|multi_level_namespace_test_with_allowlist|local_repository_test|stamping_with_stamping_off"
 echo "** Querying for tests..."
-regen_legacy_targets=$(${BAZEL} query //test:all | grep regenerate_with_jar | grep -vE "_golden_stardoc|$EXCLUDED_TESTS")
-regen_starlark_doc_extract_targets=$(${BAZEL} query //test:all | grep regenerate_ | grep -vE "_legacy|_golden\.extract|$EXCLUDED_TESTS")
+regen_starlark_doc_extract_targets=$(${BAZEL} query //test:all | grep regenerate_ | grep -vE "_golden\.extract|$EXCLUDED_TESTS")
 
 echo "** Building goldens..."
-${BAZEL} build $regen_legacy_targets $regen_starlark_doc_extract_targets
+${BAZEL} build $regen_starlark_doc_extract_targets
 
 echo "** Copying starlark_doc_extract goldens..."
 for regen_target in $regen_starlark_doc_extract_targets; do
@@ -65,30 +64,6 @@ for regen_target in $regen_starlark_doc_extract_targets; do
   golden="test/testdata/${testdata_pkg_name}/golden.${ext}"
   cp "${out_file}" "${golden}"
   chmod 644 "${golden}"
-done
-
-echo "** Copying legacy goldens..."
-for regen_target in $regen_legacy_targets; do
-  base_target_name=$(echo $regen_target | sed 's/\/\/test://g')
-  testdata_pkg_name=$(echo $base_target_name | sed 's/regenerate_with_jar_//g' | sed 's/_legacy_golden//g')
-  out_file="bazel-bin/test/${base_target_name}.out"
-  if [[ $regen_target == *"proto_format"* ]]; then
-    ext="binaryproto"
-  else
-    ext="md"
-  fi
-  golden="test/testdata/${testdata_pkg_name}/golden.${ext}"
-  legacy_golden="test/testdata/${testdata_pkg_name}/legacy_golden.${ext}"
-  legacy_golden_target="${legacy_golden#test/}"
-  if diff "${out_file}" "${golden}" > /dev/null; then
-    # legacy_golden is no longer needed; we only need golden
-    [[ -e "${legacy_golden}" ]] && rm "${legacy_golden}"
-    run_buildozer "remove legacy_golden_file" "//test:${testdata_pkg_name}"
-  else
-    cp "${out_file}" "${legacy_golden}"
-    chmod 644 "${legacy_golden}"
-    run_buildozer "set legacy_golden_file \"${legacy_golden_target}\"" "//test:${testdata_pkg_name}"
-  fi
 done
 
 echo "** Files copied."
